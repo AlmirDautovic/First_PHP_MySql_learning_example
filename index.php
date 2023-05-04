4c1672915a67aded875f1fb3deb989cdb21ee8aa
AlmirDautovic <aalmir.d@gmail.com>
2023-05-05 00:11:03 +0200

    created index.php file, and inside of it i created form for registering users. added basic styles

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Example</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+        <link rel="stylesheet" href="stylesheet.css">
+</head>
+<body class="grey lighten-4">
+   <section class="container grey-text">
+    <h4 class="center">Add a User!</h4>
+    <form action="" class="white">
+        <label for="name">User Name:</label>
+        <input type="text" name="name">
+        <label for="email">Your Email:</label>
+        <input type="email" name="email">
+        <label for="age">Your Age:</label>
+        <input type="number" name="age">
+        <label for="message">Your Message:</label>
+        <input type="text" name="message">
+        <div class="center">
+            <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
+        </div>
+    </form>
+   </section>
+</body>
+</html>
