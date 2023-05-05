2b66c3e09bef0905c1bb1ed04e391a93586937f0
AlmirDautovic <aalmir.d@gmail.com>
2023-05-05 16:48:43 +0200

    added connection to mySql db and started defining db enrties for submiting

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
 
+$conn = mysqli_connect('localhost', 'almir', 'almir', 'php example');
+
+if (!$conn) {
+    echo 'Connection error: ' . mysqli_connect_error();
+}
+
+$name = $email = $age = $message = '';
+$errors = array('name' => '', 'email' => '', 'age' => '', 'message' => '');
+
+if (isset($_POST['submit'])) {
+    if (empty($_POST['name'])) {
+        $errors['name'] = 'A name is required ' + '<br>';
+    }
+}
 
 
 ?>
@@ -7,30 +21,33 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Example</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-        <link rel="stylesheet" href="stylesheet.css">
+    <link rel="stylesheet" href="stylesheet.css">
 </head>
+
 <body class="grey lighten-4">
-   <section class="container grey-text">
-    <h4 class="center">Add a User!</h4>
-    <form action="" class="white">
-        <label for="name">User Name:</label>
-        <input type="text" name="name">
-        <label for="email">Your Email:</label>
-        <input type="email" name="email">
-        <label for="age">Your Age:</label>
-        <input type="number" name="age">
-        <label for="message">Your Message:</label>
-        <input type="text" name="message">
-        <div class="center">
-            <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
-        </div>
-    </form>
-   </section>
+    <section class="container grey-text">
+        <h4 class="center">Enter Your Information!</h4>
+        <form action="<?php echo $_SERVER['PHP_SELF'] ?>" class="white" method="POST">
+            <label for="name">User Name:</label>
+            <input type="text" name="name">
+            <label for="email">Your Email:</label>
+            <input type="email" name="email">
+            <label for="age">Your Age:</label>
+            <input type="number" name="age">
+            <label for="message">Your Message:</label>
+            <input type="text" name="message">
+            <div class="center">
+                <input type="submit" name="submit" value="submit" class="btn z-depth-0">
+            </div>
+        </form>
+    </section>
 </body>
+
 </html>
